6bc8411f4c487497c312b37efcb53d4749200d03
hyunmin2 <201802142@o.cnu.ac.kr>
2023-09-20 12:09:49 +0900

    Initial commit

---
+++ src/main/java/com/project/eternal/EternalApplication.java
@@ -0,0 +1,13 @@
+package com.project.eternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EternalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/eternal/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.project.eternal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+}
